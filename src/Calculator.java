785788da5ee5c8e5378adce3e96f612e02ae5c94
aidenrosen <aidenrrosen@gmail.com>
2022-02-07 16:29:35 -0500

    Allows user to input 2 numbers and print the sum

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Calculator
 {
 	public static void main(String[] args)
 	{
-
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter 2 integers to add together.");
+		int x = in.nextInt();
+		int y = in.nextInt();
+		System.out.println(x+y);
 	}
 }
