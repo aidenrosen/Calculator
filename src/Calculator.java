633b8d5d0b6cc3f62304d4f7f73d613211a38c5a
aidenrosen <aidenrrosen@gmail.com>
2022-02-07 16:43:31 -0500

    Added support for floating point number inputs

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,9 +5,9 @@ public class Calculator
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
-		System.out.println("Please enter 2 integers to add together.");
-		int x = in.nextInt();
-		int y = in.nextInt();
+		System.out.println("Please enter 2 numbers to add together.");
+		Double x = in.nextDouble();
+		Double y = in.nextDouble();
 		System.out.println("What operation would you like to perform?  Enter the number next to the corresponding information");
 		System.out.println("1. Add");
 		System.out.println("2. Subtract");
